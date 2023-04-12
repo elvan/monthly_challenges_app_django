8c7814c66a6195c1bdea8fb2e7405dbd923b3eb7
Elvan Hidayat <elvanhd@gmail.com>
2023-04-13 03:38:29 +0700

    creating a 404 template

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
