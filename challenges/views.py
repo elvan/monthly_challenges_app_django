4e6109ab4d9a50c8a5047afd959d28a359015edd
Elvan Hidayat <elvanhd@gmail.com>
2023-04-12 23:08:34 +0700

    the title filter

--- challenges/views.py
+++ challenges/views.py
@@ -54,7 +54,7 @@ def monthly_challenge(request, month):
         return render(
             request,
             "challenges/challenge.html",
-            {"text": challenge_text, "month_name": month.capitalize()},
+            {"text": challenge_text, "month_name": month},
         )
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
